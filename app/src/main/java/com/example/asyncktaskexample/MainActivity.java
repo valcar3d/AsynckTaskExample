2198e4bab36354bb6355495e8733f5037e6a4a32
angel <valcar3d@gmail.com>
2020-09-11 14:07:44 -0500

    Added cancel functionality

--- app/src/main/java/com/example/asyncktaskexample/MainActivity.java
+++ app/src/main/java/com/example/asyncktaskexample/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.asyncktaskexample;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView = null;
     ProgressDialog progressDialog = null;
     Integer drawablePath;
+    DoSomeWorkInBackground asynckTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image);
-        Button button = findViewById(R.id.asyncTask);
+        Button btnDownload = findViewById(R.id.btnDownload);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //AsyncTasks can't be reused.
                 // You have to create a new instance every time you want to run one.
-                DoSomeWorkInBackground someWork = new DoSomeWorkInBackground();
-                someWork.execute();//Calling the AsynckTask method to execute
+                asynckTask = new DoSomeWorkInBackground();
+                asynckTask.execute();//Calling the AsynckTask method to execute
             }
         });
-
 
     }
 
@@ -46,11 +48,21 @@ public class MainActivity extends AppCompatActivity {
         //Result - the type that is returned by doInBackground().
 
         //onPreExecute - executed in the main thread to do things like create the initial progress bar view.
+        //or Cancel the execution
         @Override
         protected void onPreExecute() {
             progressDialog = new ProgressDialog(MainActivity.this);
             progressDialog.setMessage("Downloading...wait");
-            progressDialog.setCancelable(false);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(true);
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    asynckTask.cancel(true);
+                    //Click anywhere to cancel download
+                    Toast.makeText(MainActivity.this, "Download canceled", Toast.LENGTH_LONG).show();
+                }
+            });
             progressDialog.show();
         }
 
@@ -61,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             //Use any method that needs to run an extended period of time here
             try {
                 //Simulate a "long running operation"
-                Thread.sleep(3000);
+                Thread.sleep(6000);
                 drawablePath = R.drawable.xpsimage;
 
             } catch (InterruptedException e) {
